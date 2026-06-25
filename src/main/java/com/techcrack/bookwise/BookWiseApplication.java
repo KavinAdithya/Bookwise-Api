210a3af3987f6d63ad846936a4ebdeac08ef070e
KaVin S <kavinadithya3@gmail.com>
2026-06-25 08:22:16 +0530

    Project dependencie and structure set up done.

---
+++ src/main/java/com/techcrack/bookwise/BookWiseApplication.java
@@ -0,0 +1,13 @@
+package com.techcrack.bookwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techcrack/bookwise/BookWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcrack.bookwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
