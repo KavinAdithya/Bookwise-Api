5ea9e9b01a67bd89618cd67afa7032663df4ce1c
KaVin S <kavinadithya3@gmail.com>
2026-07-10 09:06:59 +0530

    Author Authentication Failed due to double hashing bug

--- src/main/java/com/techcrack/bookwise/service/AuthorService.java
+++ src/main/java/com/techcrack/bookwise/service/AuthorService.java
@@ -1,7 +1,6 @@
 package com.techcrack.bookwise.service;
 
 import com.techcrack.bookwise.entity.Author;
-import com.techcrack.bookwise.entity.Users;
 import com.techcrack.bookwise.repository.AuthorRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,19 +11,15 @@ public class AuthorService {
 
     private final AuthorRepository repo;
     private final Logger logger;
-    private final UserService userService;
 
-    public AuthorService(AuthorRepository repo, UserService userService) {
+    public AuthorService(AuthorRepository repo) {
         this.repo = repo;
-        this.userService = userService;
         this.logger = LoggerFactory.getLogger(AuthorService.class);
     }
 
     public Author register(Author author) {
         logger.info("Registration process for author has started {}", author.getUser().getUsername());
 
-        Users user = userService.register(author.getUser());
-        author.setUser(user);
         author = repo.save(author);
         
         logger.info("Registration process for author has completed {} and moved for admin verification", author.getUser().getUsername());
